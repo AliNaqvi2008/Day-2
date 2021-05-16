cf70773051110130a16db1974f0503f4f9085751
AliNaqvi2008 <84286239+AliNaqvi2008@users.noreply.github.com>
2021-05-16 19:12:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,187 @@
+console.log("Numbers in js") // STRINGS
+
+// let num1= parseInt(prompt("Enter your first number")); // FLOATS
+// let num2= parseInt(prompt("Enter your second number")); // INTEGERS
+// let num3= parseInt(prompt("Enter your third number")); // INTEGERS
+// console.log("addition = " ,num1+num2+num3);
+// console.log("subtraction = " ,num1-num2-num3);
+// console.log("division = " ,num1/num2/num3);
+// console.log("multiplaction = " ,num1*num2*num3);
+// console.log("remainder = " ,num1%num2%num3);
+// let addition =num1+num2+num3;
+// let subtraction =num1-num2-num3;
+// let division =num1/num2/num3;
+// let multiplacation =num1*num2*num3;
+// let remainder =num1%num2%num3;
+
+// document.getElementById("addition").innerHTML=addition
+// document.getElementById("subtraction").innerHTML=subtraction
+// document.getElementById("division").innerHTML=division
+// document.getElementById("multiplacation").innerHTML=multiplacation
+// document.getElementById("remainder").innerHTML=remainder
+// alert("Thank You For Your Services!")
+
+// let numa= 10;
+// let numb= 10;
+// let numc= 10;
+// let numd= 10;
+
+// numa -=5; //Decremental operation
+// numb +=5; //Incremental operation
+// numc /=5; //Decremental operation
+// numd *=5; //Incremental operation
+// console.log(numa,numb,numc,numd)
+// let num3= 10
+
+// num3 -=5;
+// console.log(num3)
+
+// let a = "ali";
+// let b="naqvi"
+// let space=" "
+// console.log(a+ space +b)
+
+// function studentdate(){
+//   let a = 2;
+//   let a = 2;
+//   let a = 2;
+//   let a = 2;
+
+//     console.log(a+b+c+d)
+//     console.log(a+b+c+d)
+//     console.log(a+b+c+d)
+//     console.log(a+b+c+d)
+// }
+
+// function calc(num1,num2,num3){
+// let sum = num1 + num2 + num3 
+// let sub = num1 - num2 - num3
+// let mul = num1 * num2 * num3
+// let div = num1 / num2 / num3
+// let rem = num1 % num2 % num3
+// return(console.log(sum ,sub ,mul ,div ,rem))
+
+// }
+// let a = parseInt(prompt("Enter first number"))
+// let b = parseInt(prompt("Enter second number"))
+// let c = parseInt(prompt("Enter third number"))
+// calc(a,b,c)
+
+// function name1(firstname , lastname ,age){
+//   let fullname= firstname + ' ' + lastname
+  
+// return(console.log(fullname , "I am" , age , "years old"))
+// } 
+// name1("Ali","Naqvi." ,12)
+
+// function hyp(a,b){
+// let c = Math.pow(a,2) + Math.pow(b,2)
+
+// return(console.log(c))
+// }
+// hyp(4,4)
+
+//Loops have two types
+//For loops
+//While loops
+// console.log("Table of 2")
+// let num = 0; // Starting Point
+
+// while( num < 20 ){ // While body
+//     num +=2;
+// console.log(num)
+
+// }
+
+// console.log("Table of 3")
+// let num2 = 0; // Starting Point
+
+// while( num2 < 30 ){ // While body
+//   num2 +=3;
+// console.log(num2)
+
+// }
+
+// console.log("Table of 4")
+// let num3 = 0; // Starting Point
+
+// while( num3 < 40 ){ // While body
+//   num3 +=4;
+// console.log(num3)
+
+// }
+
+// console.log("Table of 5")
+// let num4 = 0; // Starting Point
+
+// while( num4 < 50 ){ // While body
+//   num4 +=5;
+// console.log(num4)
+
+// }
+
+// let numbers = parseInt(prompt("Enter number"));
+
+// for(let num=1 ; num <11 ; num++){
+ 
+// let result =numbers*num;
+    
+// console.log(result)
+// document.getElementById("multitable").innerHTML=result;
+
+// }
+
+// let x = 0;
+
+// while(x<10){
+//     x++;
+//     console.log(x)
+// }
+// let num = prompt("enter");
+
+// for(let i=2; i<11 ; i++){
+//     result = num*i;
+
+// document.write(result)
+// }
+//Data types
+
+// let numbers=12;
+// let string="Ali";
+// let object={firstname:"ali ", lastname:"naqvi", hobbbies:"swimming" ,age:12}
+// let float= 12.3;
+// let boolean= true;
+// let array=["apple","banana","mango"]
+// let random;
+// let nothing=null;
+// //string
+
+// let sport="swimming";
+// let moresports="hockey\nfootball"
+// let test="Pakistan commemorating"
+// console.log(sport.length)
+// console.log(moresports.length)
+// console.log(moresports.indexOf("hockey"))
+// console.log(test.slice(17,19))
+// console.log(moresports.replace("hockey","tennis"))
+
+// let a=prompt("Enter a number " ,"0");
+// for(let s=1;s<=10;s++)
+// {
+//     document.write(a+"x"+s+"="+a*s+"<br/r>");
+// }
+
+// let fruit="apple"
+// console.log(para.toUpperCase())
+// console.log(para.toLowerCase())
+// console.log(para.charAt(10))
+// console.log(fruit.split(''))
+// console.log(sport.length)
+
+// let para="Pakistan, officially the Islamic Republic of Pakistan, is a country in South Asia. It is the world's fifth-most populous country with a population exceeding 212.2 million, and has the world's second-largest Muslim population."
+// console.log(para.length)
+// console.log(para.indexOf("population"))
+// console.log(para.slice(157,162))
+// console.log(para.replace("world's second-largest","world's first-largest"))
+// document.write(para.length + "<br>" + para.indexOf("population") + "<br>" + para.slice(157,162) + "<br>" + para.replace("world's second-largest","world's first-largest"));
+
